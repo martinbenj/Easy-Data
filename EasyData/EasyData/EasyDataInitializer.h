1f52c351a1eace01eb221d9e320952d03eb3110b
Ben Martin <ben@martinben.com>
2014-05-24 23:47:26 -0700

    Initial commit

---
+++ EasyData/EasyData/EasyDataInitializer.h
@@ -0,0 +1,13 @@
+//
+//  EasyDataInitializer.h
+//  EasyData
+//
+//  Created by Benjamin Martin on 5/24/14.
+//  Copyright (c) 2014 Benjamin Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyDataInitializer : NSObject
+
+@end
---
+++ EasyData/EasyData/EasyDataInitializer.m
@@ -0,0 +1,13 @@
+//
+//  EasyDataInitializer.m
+//  EasyData
+//
+//  Created by Benjamin Martin on 5/24/14.
+//  Copyright (c) 2014 Benjamin Martin. All rights reserved.
+//
+
+#import "EasyDataInitializer.h"
+
+@implementation EasyDataInitializer
+
+@end
